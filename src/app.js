6c892c21a6a96ac596745a9031c5b89574b8a182
Kdvergarab <kdvergarab@gmail.com>
2022-02-24 14:16:59 +0000

    avance3

--- src/app.js
+++ src/app.js
@@ -5,13 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./style.css";
 
-window.onload = () => {
-  document.querySelector(".card").classList.add("generateRandompinta");
-  document.querySelector(".card").innerHTML.add = generateRandomNumber();
-};
+// window.onload = () => {
+//   document.querySelector(".card").classList.add(generateRandompinta());
+//   document.querySelector(".card").innerHTML = generateRandomNumber();
+// };
 
-let generateRandomNumber = () => {
-  let numbers = [
+// let generateRandomNumber = () => {
+//   let numbers = [
+//     "A",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "10",
+//     "j",
+//     "Q",
+//     "K"
+//   ];
+//   let indexnumbers = Math.floor(Math.random() * numbers.length);
+//   return numbers[indexnumbers];
+// };
+
+// let generateRandompinta = () => {
+//   let pinta = ["diamante", "picas", "corazon", "trebol"];
+//   let indexpinta = Math.floor(Math.random() * pinta.length);
+
+//   return pinta[indexpinta];
+// };
+
+window.onload = () => {
+  let numeros = [
     "A",
     "2",
     "3",
@@ -26,13 +53,13 @@ let generateRandomNumber = () => {
     "Q",
     "K"
   ];
-  let indexnumbers = Math.floor(math.random() * numbers.length);
-  return numbers[indexnumbers];
-};
 
-let generateRandompinta = () => {
-  let pinta = ["diamante", "picas", "corazon", "trebol"];
-  let indexpinta = Math.floor(math.random() * pinta.length);
-
-  return pinta[indexpinta];
+  let numeroselecionado = numeros[Math.floor(Math.random() * (13 - 0))];
+document.getElementById("numero").innerHTML =x;
 };
+<p id="demo"></p>
+
+<script>
+let x = Math.floor((Math.random() * 100) + 1);
+document.getElementById("demo").innerHTML = x;
+</script>
