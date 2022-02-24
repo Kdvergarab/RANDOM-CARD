26342f3fb4715eb52811e07581b2a751921e7f56
Kdvergarab <kdvergarab@gmail.com>
2022-02-24 20:22:43 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -37,29 +37,62 @@ import "./style.css";
 //   return pinta[indexpinta];
 // };
 
-window.onload = () => {
-  let numeros = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "j",
-    "Q",
-    "K"
-  ];
+function randomnumero(numeros) {
+  return numeros[Math.floor(Math.random() * numeros.length)];
+}
 
-  let numeroselecionado = numeros[Math.floor(Math.random() * (13 - 0))];
-document.getElementById("numero").innerHTML =x;
-};
-<p id="demo"></p>
+let numeros = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "j",
+  "Q",
+  "K"
+];
+// console.log(randomnumero(numeros));
+
+document.getElementById("numero").innerHTML = randomnumero(numeros);
+
+function randompinta(pinta) {
+  return pinta[Math.floor(Math.random() * pinta.length)];
+}
+
+let pinta = ["♣", "♠", "♥️", "♦️"];
+// console.log(pinta);
+
+let pintaseleccionada = [randompinta(pinta)];
+
+if (pintaseleccionada == "♥️") {
+  document.getElementById("arriba-pinta").style.color = "red";
+  document.getElementById("abajo-pinta").style.color = "red";
+} else if (pintaseleccionada == "♦️") {
+  document.getElementById("arriba-pinta").style.color = "red";
+  document.getElementById("abajo-pinta").style.color = "red";
+}
+
+document.getElementById("arriba-pinta").innerHTML = pintaseleccionada;
+document.getElementById("abajo-pinta").innerHTML = pintaseleccionada;
+
+// function random_item(items)
+// {
+
+// return items[Math.floor(Math.random()*items.length)];
+
+// }
+
+// var items = [254, 45, 212, 365, 2543];
+// console.log(random_item(items));
+
+/* <p id="demo"></p>
 
 <script>
 let x = Math.floor((Math.random() * 100) + 1);
 document.getElementById("demo").innerHTML = x;
-</script>
+</script> */
